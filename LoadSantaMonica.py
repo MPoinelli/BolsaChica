0ead68a9540453752f76eada733bb048595d8bec
MPoinelli <m.poinelli@tudelft.nl>
2022-04-23 22:19:08 -0700

    Add type of data and description of columns

--- LoadSantaMonica.py
+++ LoadSantaMonica.py
@@ -1,10 +1,11 @@
 import datetime
 import pandas as pd
 import cartopy
+import matplotlib.pyplot as plt
 
 # Getting the current date and time of today and yesterday (defined as 24 hours ago)
 today = datetime.datetime.utcnow().replace(microsecond=0, second=0, minute=0)
-yesterday = datetime.datetime.utcnow().replace(microsecond=0, second=0, minute=0) - datetime.timedelta(days=1)
+yesterday = datetime.datetime.utcnow().replace(microsecond=0, second=0, minute=0) - datetime.timedelta(days=10)
 
 today_url = today.strftime("%Y-%m-%dT%H:%M:%S").replace(':','%3A')
 yesterday_url = yesterday.strftime("%Y-%m-%dT%H:%M:%S").replace(':','%3A')
@@ -18,4 +19,13 @@ URL_time = "time%3E" + yesterday_url + "Z&time%3C" + today_url +'Z&'
 URL_order = "waveFlagPrimary=1&orderByMax(%22time%2Cstation_id%22)"
 
 URL = URL_erdapp + URL_parameters + URL_stationID + URL_time + URL_order
-df = pd.read_csv(URL)
+df = pd.read_csv(URL, header=[0,1], dtype={'time':str, 'waveHs': float, 'waveTp': float, 'waveTa': float, 'waveDp': float})
+
+## Description of data:
+# waveHs: waveHs (significant wave height, meter)
+# waveTp (peak wave period, second)
+# waveTa (average wave period, second)
+# waveDp (peak wave direction, degreeT)
+
+plt.plot(df['waveHs'])
+plt.show()
