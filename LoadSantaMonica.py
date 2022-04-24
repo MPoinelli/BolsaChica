02fdc34a025ca879b17471e854036232ff4df6d7
MPoinelli <m.poinelli@tudelft.nl>
2022-04-23 21:46:19 -0700

    Add URL datetime and yesterday time

--- LoadSantaMonica.py
+++ LoadSantaMonica.py
@@ -2,9 +2,15 @@ import datetime
 import pandas as pd
 import cartopy
 
-# Getting the current date
-# and time
-now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+# Getting the current date and time of today and yesterday (defined as 24 hours ago)
+today = datetime.datetime.utcnow().replace(microsecond=0, second=0, minute=0)
+yesterday = datetime.datetime.utcnow().replace(microsecond=0, second=0, minute=0) - datetime.timedelta(days=1)
+
+today_url = today.strftime("%Y-%m-%dT%H:%M:%S").replace(':','%3A')
+yesterday_url = yesterday.strftime("%Y-%m-%dT%H:%M:%S").replace(':','%3A')
+
+print(today_url)
+print(yesterday_url)
 
 # Create URL
 URL_erdapp = "http://erddap.cdip.ucsd.edu/erddap/tabledap/"
@@ -15,3 +21,5 @@ URL_order = "waveFlagPrimary=1&orderByMax(%22time%2Cstation_id%22)"
 
 URL = URL_erdapp + URL_parameters + URL_stationID + URL_time + URL_order
 df = pd.read_csv(URL)
+
+
