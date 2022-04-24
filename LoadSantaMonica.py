e227626158e1eb99d5af60f82e87c3c3b561a7e4
MPoinelli <m.poinelli@tudelft.nl>
2022-04-23 21:49:49 -0700

    Add automatic retrive of last 24 hours

--- LoadSantaMonica.py
+++ LoadSantaMonica.py
@@ -9,17 +9,13 @@ yesterday = datetime.datetime.utcnow().replace(microsecond=0, second=0, minute=0
 today_url = today.strftime("%Y-%m-%dT%H:%M:%S").replace(':','%3A')
 yesterday_url = yesterday.strftime("%Y-%m-%dT%H:%M:%S").replace(':','%3A')
 
-print(today_url)
-print(yesterday_url)
-
 # Create URL
 URL_erdapp = "http://erddap.cdip.ucsd.edu/erddap/tabledap/"
 URL_parameters = "wave_agg.csv?station_id%2Ctime%2CwaveHs%2CwaveTp%2CwaveTa%2CwaveDp&"
 URL_stationID = "station_id=%22028%22&"
-URL_time = "time%3E2022-04-10T00%3A00%3A00Z&time%3C2022-04-11T%3A00%3A00%3A00Z&"
+
+URL_time = "time%3E" + yesterday_url + "Z&time%3C" + today_url +'Z&'
 URL_order = "waveFlagPrimary=1&orderByMax(%22time%2Cstation_id%22)"
 
 URL = URL_erdapp + URL_parameters + URL_stationID + URL_time + URL_order
 df = pd.read_csv(URL)
-
-
